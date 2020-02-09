d86e7b4ddaf20e7771f3f9978b7baa39d0b9d11a
Omer <Omer231195@gmail.com>
2020-02-09 14:05:36 +0200

    Fifth

--- api/Entrypoint.sh
+++ api/Entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-while ! wget db:
+until nc -z -v -w30 db 5432; do
+    echo "Waiting for database connection..."
+    # wait for 5 seconds before check again
+    sleep 5
+done
+
 npx sequelize-cli db:migrate 
 npx sequelize-cli db:seed:all 
-npm start 
+npm start
